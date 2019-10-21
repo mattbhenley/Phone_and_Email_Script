510c296d189ae0b680a30716d8098d58025c2e10
matt henley <mattbhenley@gmail.com>
2019-10-20 20:45:13 -0500

    deleted whitespace and fixed spelling error

--- phoneAndEmail.py
+++ phoneAndEmail.py
@@ -6,7 +6,7 @@ import pyperclip, re
 phoneRegex = re.compile(r'''(
     (\d{3}|\(\d{3}\))?  # area code 
     (\s|-|\.)?          # separator 
-    (\d{3})             # forst 3 digits
+    (\d{3})             # first 3 digits
     (\s|-|\.)           # separator 
     (\d{4})             # last 4 digits
     (\s*(ext|x|ext.)\s*(\d{2,5}))?  # extension
@@ -15,9 +15,9 @@ phoneRegex = re.compile(r'''(
 # Todo: Create an email regex 
 emailRegex = re.compile(r'''(
     [a-zA-Z0-9._%+-]+   # username
-    @                   # symbol
+    @                   # @ symbol
     [a-zA-Z0-9.-]+      # domain name 
-    (\.[a-zA-Z]{2, 4})  # dot-something
+    (\.[a-zA-Z]{2,4})  # dot-something
     )''', re.VERBOSE)
 
 # Todo: Find matches in clipboard text 
